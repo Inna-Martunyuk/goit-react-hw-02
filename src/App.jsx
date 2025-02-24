0b0009cfa94b278fb941b4cbcb4cb3e7a0ac2306
Inna-Martunyuk <imartunyuk88@gmail.com>
2025-02-24 18:12:22 +0200

    hw-2-no-style

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import Feedback from "./components/Feedback/Feedback"
+import Options from "./components/Options/Options"
+import Notification from "./components/Notification/Notification"
+import { useState } from "react";
+
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+   const resetFeedback = () => {
+     setFeedback({ good: 0, neutral: 0, bad: 0 });
+   };
+  const positiveFeedback = Math.round((feedback.good  / totalFeedback) * 100);
+
+  return (
+    <>
+      <h1>Sip Happens Café</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
+    </>
+  );
+};
+
+export default App;
+ 
+   
+  
+ 
